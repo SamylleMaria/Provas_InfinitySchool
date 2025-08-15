18dd963a4c5a49be0a8e02cb4ef3576cfac9d41d
Samylle Souza <samyllemaria@outlook.com>
2025-08-15 17:49:26 -0300

    Update prova_revisão_python.py
    Correção de estrutura de comentário

--- prova_revisão_python.py
+++ prova_revisão_python.py
@@ -1,4 +1,4 @@
-'''Crie um dicionário para armazenar o nome e o preço de cinco produtos. Peça ao usuário para inserir o nome de cada produto e o respectivo preço. À medida que o usuário fornece os dados, armazene cada produto como uma chave no dicionário e o preço como o valor associado a essa chave. Após a inserção de todos os produtos e preços, calcule o valor total da compra somando todos os preços armazenados no dicionário. Por fim, exiba o valor total da compra.'''
+#Crie um dicionário para armazenar o nome e o preço de cinco produtos. Peça ao usuário para inserir o nome de cada produto e o respectivo preço. À medida que o usuário fornece os dados, armazene cada produto como uma chave no dicionário e o preço como o valor associado a essa chave. Após a inserção de todos os produtos e preços, calcule o valor total da compra somando todos os preços armazenados no dicionário. Por fim, exiba o valor total da compra.'''
 
 produtos ={}
 valor_total_da_compra = 0
